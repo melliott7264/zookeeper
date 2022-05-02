49586e278c3b38540f53514646013414cb4f5dee
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-05-02 10:41:09 -0400

    added support for single animal query

--- server.js
+++ server.js
@@ -56,6 +56,11 @@ function filterByQuery(query, animalsArray) {
 
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res)=> {
     let results = animals;
     
@@ -64,6 +69,16 @@ app.get('/api/animals', (req, res)=> {
     }
 
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result =findById(req.params.id, animals);
+    if (result) {
+       res.json(result); 
+    } else {
+        res.sendStatus(404);
+    }
+    
 });
 
 app.listen(PORT, () => {
