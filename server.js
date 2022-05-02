18e609538dfb80d74f4071ba4ea14799b9fa4ede
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-05-02 10:13:15 -0400

    updated port for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const {animals} = require('./data/animals');
 
+// set port for Heroku, if available, or 3001 by default
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
